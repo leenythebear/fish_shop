5deed49b3e21ef5ec9520961878b6168eb410420
leeny_the_bear <leeny@inbox.ru>
2023-01-09 16:33:44 +0600

    make the function for create the cart

--- main.py
+++ main.py
@@ -26,13 +26,34 @@ def get_products(token):
     return response.json()
 
 
+def create_cart(token):
+    carts_url = 'https://api.moltin.com/v2/carts'
+    headers = {
+        'Authorization': 'Bearer {}'.format(token),
+        'Content-Type': 'application/json',
+    }
+    data = {
+        'data': {
+            "name": "Bob’s cart",
+        }
+    }
+    response = requests.post(carts_url, headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()['id']
+
+
 if __name__ == '__main__':
     load_dotenv()
     client_id = os.environ['CLIENT_ID']
     client_secret = os.environ['CLIENT_SECRET']
-    # token = os.environ['TOKEN']
     token = get_token(client_id, client_secret)
-    print(get_products(token))
-    print(token)
+    products = get_products(token)
+    product = products['data'][0]
+    cart_id = create_cart(token)
+    # cart = add_product_to_cart(client_id, token, product)
+    print(create_cart(token))
+
+
+
 
 
