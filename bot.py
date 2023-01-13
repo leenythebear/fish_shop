455bc73e91c477bb6528c3f6cf004a73ae328a94
leeny_the_bear <leeny@inbox.ru>
2023-01-13 19:25:58 +0600

    make a function for handle users reply

--- bot.py
+++ bot.py
@@ -80,53 +80,36 @@ def get_database_connection():
     поэтому по этой фразе выставляется стартовое состояние.
     Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
     """
-    # db = get_database_connection()
-    users_reply = update.callback_query
-    if users_reply.data == 'cart':
-        message = 'Вы перешли в корзину'
-        bot.send_message(text=message,
-                         chat_id=users_reply.message.chat_id)
-    # if update.message:
-    #     user_reply = update.message.text
-    #     chat_id = update.message.chat_id
-    # elif update.callback_query:
-    #     user_reply = update.callback_query.data
-    #     chat_id = update.callback_query.message.chat_id
-    # else:
-    #     return
-    # if user_reply == '/start':
-    #     user_state = 'START'
-    # else:
-    #     user_state = db.get(chat_id).decode("utf-8")
-    #
+    global _database
+    if _database is None:
+        database_password = os.getenv("DATABASE_PASSWORD")
+        database_host = os.getenv("DATABASE_HOST")
+        database_port = os.getenv("DATABASE_PORT")
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
+
+
+def handle_users_reply(update, context):
+    db = context.bot_data['db']
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode("utf-8")
     states_functions = {
         'START': start,
         'HANDLE_MENU': handle_menu,
     }
-
-    # state_handler = states_functions[user_state]
-    # Если вы вдруг не заметите, что python-telegram-bot перехватывает ошибки.
-    # Оставляю этот try...except, чтобы код не падал молча.
-    # Этот фрагмент можно переписать.
-    # try:
-    #     next_state = state_handler(bot, update)
-    #     db.set(chat_id, next_state)
-    # except Exception as err:
-    #     print(err)
-#
-#
-# def get_database_connection():
-#     """
-#     Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
-#     """
-#     global _database
-#     if _database is None:
-#         database_password = os.getenv("DATABASE_PASSWORD")
-#         database_host = os.getenv("DATABASE_HOST")
-#         database_port = os.getenv("DATABASE_PORT")
-#         _database = redis.Redis(host=database_host, port=database_port,
-#                                 password=database_password)
-#     return _database
+    state_handler = states_functions[user_state]
+    next_state = state_handler(update, context)
+    db.set(chat_id, next_state)
 
 
 if __name__ == '__main__':
